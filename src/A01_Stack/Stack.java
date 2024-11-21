88a4c5e857f425e14046013cd7663d0124d7778c
MarcoHugeri <11840816@edu.campus02.at>
2024-11-21 15:12:09 +0100

    added stack

--- src/A01_Stack/Stack.java
+++ src/A01_Stack/Stack.java
@@ -3,30 +3,44 @@ package A01_Stack;
 
 public class Stack<T>
 {
-	 private Node<T> first;
+    private Node<T> first;
+    private int counter;
     /**
      * Oberstes Element entfernen und zurückliefern.
      * Existiert kein Element, wird eine Exception ausgelöst.
-     * @throws StackEmptyException 
+     * @throws StackEmptyException
      */
     public T pop() throws StackEmptyException {
 
-    	return null;
+        if (counter == 0)
+            throw new StackEmptyException();
+        else {
+            Node<T> copy = first;
+            first = first.getNext();
+
+            counter--;
+            return copy.getData();
+        }
     }
-    
+
     /**
      * Übergebenen T auf Stack (als oberstes Element) speichern.
      * @param i data
      */
     public void push(T i) {
 
+        Node<T> newNode = new Node<>(i);
+        newNode.setNext(first);
+        first = newNode;
+        counter ++;
+
     }
-    
+
     /**
      * Liefert die Anzahl der Elemente im Stack
      * @return
      */
     public int getCount() {
-    	return 0;
+        return counter;
     }
 }
