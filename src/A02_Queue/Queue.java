34d0348b07ecd018571885f8e791ed4617740922
MarcoHugeri <11840816@edu.campus02.at>
2024-11-28 10:07:45 +0100

    added queue

--- src/A02_Queue/Queue.java
+++ src/A02_Queue/Queue.java
@@ -3,30 +3,30 @@ package A02_Queue;
 public class Queue<T>
 {
     private Node<T> first;
-    
+
     private Node<T> last;
     private int counter = 0;
+
     /**
      * Das vorderste (=erste) Element aus der Queue entfernen und zurückliefern.
      * Existiert kein Element, wird eine Exception ausgelöst.
-     * @throws QueueEmptyException 
+     * @throws QueueEmptyException
      */
     public T dequeue() throws QueueEmptyException {
 
         Node<T> newNode = first;
 
-        if(counter==0)
+        if (counter == 0)
             throw new QueueEmptyException();
-
 
         first = first.getNext();
         counter--;
 
-    	return newNode.getData();
+        return newNode.getData();
     }
-    
-    
-    
+
+
+
     /**
      * Übergebenen Integer am Ende der Queue anhängen.
      * @param i Zahl
@@ -35,24 +35,22 @@ public class Queue<T>
 
         Node<T> newNode = new Node<>(i);
 
-        if(counter == 0) {
+        if(counter == 0)
             first = newNode;
-            last = newNode;
-        }
+        else
+            last.setNext(newNode);
 
-        if(counter == 1)
-            first.setNext(newNode);
 
-        last.setNext(newNode);
         last = newNode;
         counter ++;
+
     }
-    
+
     /**
      * Liefert die Anzahl der Elemente im Stack
      * @return
      */
     public int getCount() {
-    	return counter;
+        return counter;
     }
 }
